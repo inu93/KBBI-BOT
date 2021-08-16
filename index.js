c0e13daa10b9ded1fd5fdb9898adbf9939fd22a4
inu93 <82743280+inu93@users.noreply.github.com>
2021-08-16 20:26:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.command('start', (ctx) => ctx.reply(process.env.start_text))
 
 bot.on('message', (ctx) => {
     let query = ctx.message.text;
-    const keyboard = new InlineKeyboard().url('Lihat diweb kbbi', 'https://kbbi.kemdikbud.go.id/entri/'+query)                                                                                                                                                                
+    const keyboard = new InlineKeyboard().url('Lihat diweb kbbi', 'https://kbbi.web.id/'+query)                                                                                                                                                                
     kbbi(query).then(res => {
      ctx.reply('Kata: '+query+'\nDitemukan: '+res.data.arti, {
         reply_markup: keyboard
